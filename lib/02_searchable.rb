5b221b0498b1ffd3a22a53e883db6c188c99c75e
Elliot Reed <elliotreed2@gmail.com>
2015-03-06 14:02:40 -0800

    finish Phase II: searchability

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,23 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    param_array = params.to_a #can't count on params.keys and params.values being in same order.
+    cols = param_array.map { |element| element[0]}
+    args = param_array.map { |element| element[1]}
+    col_string = cols.join(' = ? AND ') + " = ?"
+
+    results = DBConnection.execute(<<-SQL, *args)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{col_string}
+    SQL
+    results.map { |result| self.new(result) }
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
